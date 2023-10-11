bd80fdfb1fc6f4501b3fdbc469c8781ed103f3c5
viniciussilva512 <marvosvinicius128@gmail.com>
2023-10-11 11:43:57 -0300

    adicionei atividade05

---
+++ atividade05.py
@@ -0,0 +1,6 @@
+# Digite uma metragem e veja isso em cm e mm:
+metragem = float(input("digite a metragem"))
+centimetro = (metragem / 10)
+print("em centimetros" , centimetro)
+milimetro = (metragem / 100)
+print("em milimetros", milimetro)
